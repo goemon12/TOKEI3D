4da55f6f8d65937883e7e4257e7c824b9682769d
Tadahiro Kato <goemon12.tc4@gmail.com>
2019-08-15 07:04:36 +0900

    Initial Commit

---
+++ TOKEI3D/GameViewController.swift
@@ -0,0 +1,36 @@
+//
+//  GameViewController.swift
+//  TOKEI3D
+//
+//  Created by goemon12 on 2019/08/14.
+//  Copyright © 2019 goemon12. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // create a new scene
+        let scene = SCNScene(named: "art.scnassets/tokei3d.scn")!
+        
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // set the scene to the view
+        scnView.scene = scene
+        
+        // allows the user to manipulate the camera
+        scnView.allowsCameraControl = true
+        
+        // show statistics such as fps and timing information
+        scnView.showsStatistics = true
+        
+        // configure the view
+        scnView.backgroundColor = UIColor.black
+    }
+}
